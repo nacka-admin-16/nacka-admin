938063e15d735d6617c469ef7ec35530f27b60db
nacka-admin-16 <carl.schele@nackademin.se>
2016-09-06 14:36:24 +0200

    adding project

---
+++ AdminExamples/Examples/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AdminExamples.Examples
+{
+    public class Repository
+    {
+        public static bool IsVowel(string letter)
+        {
+            var vowels = new List<string> { "a", "e", "i", "o", "u", "y", "å", "ä", "ö" };
+
+            return vowels.Contains(letter);
+            //return vowels.Any(vowel => letter == vowel);
+        }
+    }
+}
---
+++ AdminExamples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AdminExamples.Examples;
+
+namespace AdminExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en bokstav");
+            Console.WriteLine();
+
+            var letter = Console.ReadLine();
+            var isVowel = Repository.IsVowel(letter);
+            var message = isVowel ? "Detta är en vokal" : "Detta är inte en vokal";
+
+            Console.WriteLine();
+            Console.WriteLine(message);
+
+            Console.WriteLine();
+            Console.WriteLine("Tryck enter för att avsluta programmet");
+            Console.ReadLine();
+        }
+    }
+}
